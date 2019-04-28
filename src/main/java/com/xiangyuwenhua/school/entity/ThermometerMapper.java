90f673c76d2b40ff6ff166720c69bb9b892b11b6
djw <13292353138@163.com>
2019-04-28 22:33:04 +0800

    change 增加addTM接口

--- src/main/java/com/xiangyuwenhua/school/entity/ThermometerMapper.java
+++ src/main/java/com/xiangyuwenhua/school/entity/ThermometerMapper.java
@@ -1,5 +1,8 @@
 package com.xiangyuwenhua.school.entity;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface ThermometerMapper {
     int addThermometer(Thermometer thermometer);
 }
